6de64d3b39c0acd0dfca218bee65a4624c4cd02f
jeremyfelt <jeremy.felt@gmail.com>
2024-07-25 12:32:59 -0700

    Add initial PostCSS configuration

---
+++ postcss-config/postcss.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+	plugins: {
+		'postcss-import': {
+			plugins: [ require( 'stylelint' ) ],
+		},
+		'postcss-preset-env': {
+			stage: 2,
+			features: {
+				'nesting-rules': true
+			}
+		},
+	},
+};
