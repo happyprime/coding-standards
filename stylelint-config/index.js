e9cd906f4e5a45b882dac2a1d5c8652382d00e8c
jeremyfelt <jeremy.felt@gmail.com>
2024-07-25 16:35:09 -0700

    Stop annoying specificity errors

--- stylelint-config/index.js
+++ stylelint-config/index.js
@@ -3,6 +3,13 @@
 module.exports = {
 	extends: 'stylelint-config-recommended',
 	rules: {
+		// Stylelint seems to have trouble properly tracking specificity
+		// between nested blocks of CSS and this generally results in
+		// false positives.
+		'no-descending-specificity': null,
+
+		// These rules don't account for WordPress's mix of `-` and `_` in
+		// class and ID names.
 		'selector-class-pattern': null,
 		'selector-id-pattern': null,
 	},
