fce98d9326eed6e2dc155ec3c4192b9046829e86
jeremyfelt <jeremy.felt@gmail.com>
2024-07-25 16:28:08 -0700

    Enforcing class/id names is kind of silly at this point

--- stylelint-config/index.js
+++ stylelint-config/index.js
@@ -3,26 +3,7 @@
 module.exports = {
 	extends: 'stylelint-config-recommended',
 	rules: {
-		'font-weight-notation': [
-			'numeric',
-			{
-				ignore: [ 'relative' ],
-			},
-		],
-		indentation: 'tab',
-		'selector-class-pattern': [
-			'^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
-			{
-				message:
-					'Selector should use lowercase and separate words with hyphens (selector-class-pattern)',
-			},
-		],
-		'selector-id-pattern': [
-			'^([a-z][a-z0-9]*)(-[a-z0-9]+)*$',
-			{
-				message:
-					'Selector should use lowercase and separate words with hyphens (selector-id-pattern)',
-			},
-		],
+		'selector-class-pattern': null,
+		'selector-id-pattern': null,
 	},
 };
