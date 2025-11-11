51e339550dae822558cc5986e4e858b01235f194
jeremyfelt <jeremy.felt@gmail.com>
2025-11-11 13:28:53 -0800

    Update ESLint 9.x

--- eslint-config/index.js
+++ eslint-config/index.js
@@ -1,51 +1,114 @@
-module.exports = {
-	root: true,
-	extends: [
-		'plugin:import/recommended',
-		'plugin:jsdoc/recommended',
-		'plugin:jsx-a11y/recommended',
-		'plugin:prettier/recommended',
-		'plugin:react/recommended',
-		'plugin:react-hooks/recommended',
-	],
-	overrides: [
-		{
-			files: ['**/*.{js,cjs,mjs}'],
+import importPlugin from 'eslint-plugin-import';
+import jsdocPlugin from 'eslint-plugin-jsdoc';
+import jsxA11yPlugin from 'eslint-plugin-jsx-a11y';
+import reactPlugin from 'eslint-plugin-react';
+import reactHooksPlugin from 'eslint-plugin-react-hooks';
+import prettierRecommended from 'eslint-plugin-prettier/recommended';
+import globals from 'globals';
+
+export default [
+	jsdocPlugin.configs['flat/recommended'],
+	{
+		plugins: {
+			import: importPlugin,
 		},
-	],
-	env: {
-		browser: true,
+		rules: {
+			...importPlugin.configs.recommended.rules,
+			'import/no-unresolved': [2, { ignore: ['^@wordpress/'] }],
+		},
 	},
-	ignorePatterns: [
-		'build/*.*',
-		'build/**/*.*',
-		'**/build/*.*',
-		'**/build/**/*.*',
-		'vendor/**/*.*',
-	],
-	parserOptions: {
-		ecmaVersion: "latest",
+	{
+		plugins: {
+			'jsx-a11y': jsxA11yPlugin,
+		},
+		rules: jsxA11yPlugin.configs.recommended.rules,
 	},
-	env: {
-		"es2024": true,
-	},
-	rules: {
-		'no-console': ['warn', { allow: ['error'] }],
-		'import/no-unresolved': [2, { ignore: ['^@wordpress/'] }],
-		'react/react-in-jsx-scope': 'off',
-		'react/prop-types': 'off',
-		'jsdoc/no-undefined-types': 'off',
-		'jsdoc/tag-lines': [
-			'error',
-			'any',
-			{
-				startLines: 1,
+	{
+		plugins: {
+			react: reactPlugin,
+		},
+		rules: {
+			...reactPlugin.configs.recommended.rules,
+			'react/react-in-jsx-scope': 'off',
+			'react/prop-types': 'off',
+		},
+		settings: {
+			react: {
+				version: '18',
 			},
+		},
+		languageOptions: {
+			parserOptions: {
+				ecmaFeatures: {
+					jsx: true,
+				},
+			},
+		},
+	},
+	{
+		plugins: {
+			'react-hooks': reactHooksPlugin,
+		},
+		rules: reactHooksPlugin.configs.recommended.rules,
+	},
+	prettierRecommended,
+
+	// Configuration for JS/MJS files.
+	{
+		files: ['**/*.{js,mjs}'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'module',
+			globals: {
+				...globals.browser,
+				...globals.es2024,
+			},
+		},
+		rules: {
+			'no-console': ['warn', { allow: ['error'] }],
+			'jsdoc/no-undefined-types': 'off',
+			'jsdoc/tag-lines': [
+				'error',
+				'any',
+				{
+					startLines: 1,
+				},
+			],
+		},
+	},
+
+	// Configuration for CJS files.
+	{
+		files: ['**/*.cjs'],
+		languageOptions: {
+			ecmaVersion: 'latest',
+			sourceType: 'commonjs',
+			globals: {
+				...globals.node,
+				...globals.es2024,
+			},
+		},
+		rules: {
+			'no-console': ['warn', { allow: ['error'] }],
+			'jsdoc/no-undefined-types': 'off',
+			'jsdoc/tag-lines': [
+				'error',
+				'any',
+				{
+					startLines: 1,
+				},
+			],
+		},
+	},
+
+	// Ignore patterns.
+	{
+		ignores: [
+			'build/*.*',
+			'build/**/*.*',
+			'**/build/*.*',
+			'**/build/**/*.*',
+			'vendor/**/*.*',
 		],
 	},
-	settings: {
-		react: {
-			version: '18',
-		},
-	}
-};
+];
